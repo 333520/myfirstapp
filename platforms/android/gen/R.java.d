df7d459dfbced834bea11929f8348ddeb95cebde
333520 <honiebao@gmail.com>
2013-12-12 13:16:14 -0500

    1
    upload the prj

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\_timwork\GitHub\myfirstapp\platforms\android\gen\com\tttmmm\myfirstapp\R.java \
+ : C:\_timwork\GitHub\myfirstapp\platforms\android\res\drawable\icon.png \
+C:\_timwork\GitHub\myfirstapp\platforms\android\res\drawable-hdpi\icon.png \
+C:\_timwork\GitHub\myfirstapp\platforms\android\res\drawable-ldpi\icon.png \
+C:\_timwork\GitHub\myfirstapp\platforms\android\res\drawable-mdpi\icon.png \
+C:\_timwork\GitHub\myfirstapp\platforms\android\res\drawable-xhdpi\icon.png \
+C:\_timwork\GitHub\myfirstapp\platforms\android\res\values\strings.xml \
+C:\_timwork\GitHub\myfirstapp\platforms\android\res\xml\config.xml \
+C:\_timwork\GitHub\myfirstapp\platforms\android\bin\AndroidManifest.xml \
